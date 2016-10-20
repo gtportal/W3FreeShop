9b34a864603acd5832153fc8ab5e050736403874
gtportal <gtportal@gtportal-MS-7817>
2016-10-20 07:43:10 +0200

    MySQLi UTF8 kapcsolat

--- html/set/start_f.php
+++ html/set/start_f.php
@@ -7,5 +7,5 @@ $MySqliLink = mysqli_connect('localhost', '#_user', '#_password', '#_db');
 if (!$MySqliLink) {
     die('Kapcsolódási hiba (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
 }
-
+if (!mysqli_set_charset($MySqliLink, 'utf8')) {die('Kapcsolódási hiba 1 ') ;}  
 //Változók
